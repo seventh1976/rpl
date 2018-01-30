3a945f1697d77ea0778abbcafa940541b3b47ed0
seventh1976 <seventh1976@gmail.com>
2018-01-30 12:11:50 +0800

    Adding bootstrapcdn and csrf_token to layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,17 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/cosmo/bootstrap.min.css">
+    <style>
+      .result-set {margin-top: 1em}
+    </style>
+    <!-- Scripts -->
+    <script>
+      window.Laravel = {!! json_encode([
+          'scrfToken' => csrf_token(),
+        ]) !!};
+    </script>
 </head>
 <body>
     <div id="app">
