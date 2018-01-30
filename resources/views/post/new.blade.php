a18b3225dce702d16ec93ba5a2727b372927f8be
seventh1976 <seventh1976@gmail.com>
2018-01-30 17:36:16 +0800

    adding fontawesome left arrow on back button

--- resources/views/post/new.blade.php
+++ resources/views/post/new.blade.php
@@ -9,7 +9,7 @@
       <h3>Create</h3>
     </div>
     <div class="col-md-7 page-action text-right">
-      <a href="{{route('posts.index')}}" class="btn btn-default btn-sm"></a>
+      <a href="{{route('posts.index')}}" class="btn btn-default btn-sm"> <i class="fa fa-arrow-left"></i> Back</a>
     </div>
   </div>
 
